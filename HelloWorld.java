874173c094162380a800cda2520223219084923c
silapada-hansda <72489263+silapada-hansda@users.noreply.github.com>
2021-06-24 17:53:42 +0530

    commit 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("in hellowrolddd program");
+		System.out.println("in hellowrolddd program from schedulers");
 
 	}
 
